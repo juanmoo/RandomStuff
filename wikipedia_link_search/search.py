b7ed6c5e4016616043d7bb9576819b165083510a
juanortiz <juanmoo@me.com>
2019-12-01 08:51:19 -0500

    Add wikipedia search

---
+++ wikipedia_link_search/search.py
@@ -0,0 +1,77 @@
+from wikipedia import *
+import os
+from pprint import pprint
+
+distances_memo_dump = 'distances_memo_dump.pl'
+distances_memo = None
+
+
+def BFS(source, target, neighbors, max_hops=10):
+    global distances_memo
+
+    if distances_memo == None and os.path.isfile(distances_memo_dump):
+        distances_memo = pickle.load(open(distances_memo_dump, "rb"))
+    elif distances_memo == None:
+        distances_memo = {}
+
+    visited = set()
+    parent = {source: None}
+    current = None
+
+    # Each state-tuple is in the form (node, distance)
+    q = [(source, 0)]
+
+    while(len(q) != 0):
+        current, current_distance = q.pop(0)
+        print('Exploring ', current)
+
+        if current not in visited and current_distance <= max_hops:
+            visited.add(current)
+            neighbor_list = neighbors(current)
+            neighbor_list.sort(key=lambda el: distances_memo.get(el, 100))
+            for n in neighbor_list:
+                if n not in visited:
+                    parent[n] = current if n not in parent else parent[n]
+                    q.append((n, current_distance + 1))
+
+                    # If target found, shortest path already found
+                    if n == target:
+                        print('Found target. Breaking!')
+                        q = []
+                        break
+
+    if target in parent:
+        reverse_path = [target]
+        while reverse_path[-1] is not None:
+            reverse_path.append(parent[reverse_path[-1]])
+        path = reverse_path[::-1][1:]
+
+        for i in range(len(path) - 1):
+            distances_memo[path[i]] = len(path) - i - 1
+        pickle.dump(distances_memo, open(distances_memo_dump, "wb"))
+
+        print('A path was found!')
+        print('The path is :', str(path))
+
+    else:
+        print("Target could not be found withing", max_hops, "steps")
+
+
+goal = 'Adolf Hitler'
+start = 'Incandescent light bulb'
+BFS(start, goal, get_links, max_hops=5)
+
+#pages_searched = []
+# for i in range(20):
+#    print('Starting item #', i, '----')
+#    start = get_random_title()
+#    BFS(start, goal, get_links, max_hops=6)
+#    pages_searched.append(start)
+#
+#search = {'pages_searched': pages_searched, 'distances': distances_memo}
+#json_search = json.dumps(search)
+# with open('search_results.json', 'w+') as f:
+#    f.write(json_search)
+#
+#
+# pprint(distances_memo)
---
+++ wikipedia_link_search/wikipedia.py
@@ -0,0 +1,70 @@
+import requests
+import json
+import pprint
+import pickle
+import os
+
+pp = pprint.PrettyPrinter(indent=4)
+
+api_url = 'https://en.wikipedia.org/w/api.php'
+
+dump_file = 'neighbors_dump.pl'
+memo = None
+
+
+def get_links(page_title):
+    global memo
+    if memo == None and os.path.isfile(dump_file):
+        memo = pickle.load(open(dump_file, "rb"))
+    elif memo == None:
+        memo = {}
+
+    if page_title in memo:
+        return memo[page_title]
+
+    params = {
+        'action': 'query',
+        'prop': 'links',
+        'titles': page_title,
+        'format': 'json',
+        'pllimit': 500
+    }
+
+    links = []
+    res = requests.get(api_url, params=params)
+
+    res_dict = json.loads(res.content)
+    if ('-1' in res_dict['query']['pages'] or not 'links' in list(res_dict['query']['pages'].values())[0]):
+        return []
+
+    link_list = [el['title']
+                 for el in list(res_dict['query']['pages'].values())[0]['links']]
+    links.extend(link_list)
+    while 'continue' in res_dict and len(res_dict['continue']['plcontinue']) > 2:
+        plcontinue = res_dict['continue']['plcontinue']
+        params.update({'plcontinue': plcontinue})
+        res = requests.get(api_url, params=params)
+        res_dict = json.loads(res.content)
+        link_list = [el['title'] for el in list(
+            res_dict['query']['pages'].values())[0]['links']]
+
+        links.extend(link_list)
+
+    memo[page_title] = links
+    pickle.dump(memo, open(dump_file, "wb"))
+
+    return links
+
+
+def get_random_title():
+    params = {
+        'action': 'query',
+        'format': 'json',
+        'list': 'random',
+        'rnfilterredir': 'nonredirects',
+        'rnnamespace': 0,
+        'rnlimit': 1
+    }
+    res = json.loads(requests.get(api_url, params=params).text)[
+        'query']['random'][0]['title']
+    return res
